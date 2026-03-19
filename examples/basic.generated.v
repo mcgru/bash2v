738484fe23bcc51540eacc88f7e69dc3850d1006
Ivan I. Grushin <mcgru@cgames.team>
2026-03-19 17:36:52 +0700

    removed some examples

--- examples/basic.generated.v
+++
@@ -1,8 +0,0 @@
-module main
-
-import bash2v.bashrt
-
-fn main() {
-	mut st := bashrt.new_state()
-	bashrt.run_exec(mut st, [bashrt.eval_word(mut st, bashrt.Word{ fragments: [bashrt.WordFragment(bashrt.LiteralFragment{ text: 'echo' })] })!, bashrt.eval_word(mut st, bashrt.Word{ fragments: [bashrt.WordFragment(bashrt.LiteralFragment{ text: 'hello' })] })!])!
-}
